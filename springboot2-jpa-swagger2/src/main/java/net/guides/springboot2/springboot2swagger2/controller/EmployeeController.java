7f235991b0f686e97d1c6300177bf12388acd29a
v.filatov <v.filatov@geoscan.aero>
2019-02-27 18:19:52 +0300

    testing application

--- springboot2-jpa-swagger2/src/main/java/net/guides/springboot2/springboot2swagger2/controller/EmployeeController.java
+++ springboot2-jpa-swagger2/src/main/java/net/guides/springboot2/springboot2swagger2/controller/EmployeeController.java
@@ -1,14 +1,15 @@
 package net.guides.springboot2.springboot2swagger2.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.validation.Valid;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import net.guides.springboot2.springboot2swagger2.exception.ResourceNotFoundException;
+import net.guides.springboot2.springboot2swagger2.model.Employee;
+import net.guides.springboot2.springboot2swagger2.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,14 +22,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import net.guides.springboot2.springboot2swagger2.exception.ResourceNotFoundException;
-import net.guides.springboot2.springboot2swagger2.model.Employee;
-import net.guides.springboot2.springboot2swagger2.repository.EmployeeRepository;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -89,6 +86,11 @@ public class EmployeeController {
     public Employee createEmployee(
             @ApiParam(value = "Employee object store in database table", required = true)
             @Valid @RequestBody Employee employee) {
+        employeeRepository.findByUsernameOrEmailId(employee.getUsername(), employee.getEmailId())
+                .ifPresent(employee1 -> {
+                    throw new RuntimeException("Employee with username or emailId exists");
+                });
+
         return employeeRepository.save(employee);
     }
 
--- springboot2-jpa-swagger2/src/main/java/net/guides/springboot2/springboot2swagger2/repository/EmployeeRepository.java
+++ springboot2-jpa-swagger2/src/main/java/net/guides/springboot2/springboot2swagger2/repository/EmployeeRepository.java
@@ -1,9 +1,8 @@
 package net.guides.springboot2.springboot2swagger2.repository;
 
+import net.guides.springboot2.springboot2swagger2.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import net.guides.springboot2.springboot2swagger2.model.Employee;
 
 import java.util.Optional;
 
@@ -11,5 +10,7 @@ import java.util.Optional;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByUsername(String username);
+
+    Optional<Employee> findByUsernameOrEmailId(String userName, String email);
 
 }
